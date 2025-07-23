54793a0243211c9352cebdb3a0cdd48d93e454c0
Lucas Adnot <lulufoot27@gmail.com>
2025-07-23 11:22:33 +0200

    Modified app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "HBienvenue sur l'application déployé via le HA Github, depuis l'image sur Dockerhub."
+    return "Bienvenue sur l'application déployé via le HA Github, depuis l'image sur Dockerhub."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
